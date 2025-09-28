928a4b0ad760979024413a1d7f6746aa7a6fdaf7
kviknakjartan <kjartanbrjann@gmail.com>
2025-09-28 13:49:10 +0000

    missing lines for graph

--- pages/Ice.py
+++ pages/Ice.py
@@ -35,3 +35,20 @@ fig1.add_trace(
         '<br>Date: %{x|%Y-%B}',
         line=dict(color='blue'))
 )
+fig1.update_layout(
+    title_text=f"Graph 5: {selected_hemisphere} sea ice {selected_variable.lower()}, 12 month moving average"
+)
+# Set x-axis title
+fig1.update_xaxes(title_text="Observation time")
+
+# Set y-axes titles
+fig1.update_yaxes(title_text=f"{selected_variable} (km<sup>2</sup>)")
+st.plotly_chart(fig1, use_container_width=True)
+st.caption("""Graph 4: Climate model ensemble annual global average temperature quantiles for four different scenarios 
+    from year 1850 to year 2100. 
+    For each of the three scenarios [SSP1-2.6](https://en.wikipedia.org/wiki/Shared_Socioeconomic_Pathways), 
+    [SSP2-4.5](https://en.wikipedia.org/wiki/Shared_Socioeconomic_Pathways) and 
+    [SSP5-8.5](https://en.wikipedia.org/wiki/Shared_Socioeconomic_Pathways) each model outputs forecast based on 
+    parameters governed by assumptions about socioeconomic factors in the future as well as physical quantities.
+    Each model also outputs estimation of historical global average temperatures based on physical quantities
+    as well as data from past atmospheric records and proxies. The instrumental record is shown for comparison.""")
