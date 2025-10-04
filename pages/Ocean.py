b99313086ff815f2221f599a1384b62e72ae7cf2
kviknakjartan <kjartanbrjann@gmail.com>
2025-10-04 06:37:11 +0000

    acidity graph

--- pages/Ocean.py
+++ pages/Ocean.py
@@ -7,7 +7,8 @@ from datetime import date
 
 from get_data import (
     get_sea_level_hist_data,
-    get_sea_level_latest_data
+    get_sea_level_latest_data,
+    get_ph_data
 )
 
 st.sidebar.header("Ocean")
@@ -125,6 +126,72 @@ st.caption(f"""Graph 2: Global mean sea level anomaly from satellite altimetry. 
     1999–2009 to a trend of 4.2 mm/year over 2014–2024 (Copernicus Climate Change Service).
     Data from [Copernicus Climate Change Service](https://climate.copernicus.eu/climate-indicators/sea-level).""")
 
+df_global, df_aloha = get_ph_data()
+
+# Create figure with secondary y-axis
+fig7 = make_subplots()
+
+x = df_global['date']
+y = df_global['value']
+
+# Add traces
+fig7.add_trace(
+    go.Scatter(x=x,
+        y=y,
+        name="Estimated global average pH",
+        hovertemplate =
+        'Value: %{y:.1f}'+
+        '<br>Date: %{x|%B %d, %Y}',
+        line=dict(color='blue', width=2))
+)
+
+y_lower = y - df_global["uncertainty"]
+y_upper = y + df_global["uncertainty"]
+
+fig7.add_trace(
+    go.Scatter(x=pd.concat([x, x[::-1]]),
+        y=pd.concat([y_upper, y_lower[::-1]]), 
+        name='Global pH uncertainty',
+        fill='toself',
+        fillcolor='rgba(0,0,255,0.2)',
+        hoverinfo="skip",
+        line=dict(color='rgba(0,0,255,0.2)', width=0.1))
+)
+
+fig7.add_trace(
+    go.Scatter(x=df_aloha.date,
+        y=df_aloha['pHcalc_25C'],
+        name="Hawaii measured pH",
+        hovertemplate =
+        'Value: %{y:.1f}'+
+        '<br>Date: %{x|%B %d, %Y}',
+        line=dict(color='red'))
+)
+
+fig7.update_layout(
+    title_text="Graph 3: Estimated global average and measured (Hawaii) ocean pH level",
+    legend=dict(
+        x=0.1,  # x-position (0.1 is near left)
+        y=0.7,  # y-position (0.9 is near top)
+        xref="container",
+        yref="container",
+        orientation = 'h'
+    )
+)
+
+# Set x-axis title
+fig7.update_xaxes(title_text="Observation time")
+
+# Set y-axes titles
+fig7.update_yaxes(title_text="Acidity (pH)")
+st.plotly_chart(fig7, use_container_width=True)
+st.caption("""Graph 3: Estimated global average and measured (Hawaii) ocean pH level. The ocean has absorbed roughly 20-30% of 
+    total anthropogenic carbon dioxide emissions since the 1980’s. 
+    This is causing acidification of the oceans at a rate faster than any time in the past 300 million years (Copernicus). 
+    pH is measured on a logarithmic scale which means that ocean acidity has increased by 40% since the pre-industrial era. 
+    Global average estimation data from [Copernicus](https://marine.copernicus.eu/ocean-climate-portal/ocean-acidification). 
+    Measured pH data from [Hawaii Ocean Time-series (HOT)](https://hahana.soest.hawaii.edu/hot/hotco2/hotco2.html).""")
+
 st.markdown("# References")
 
 st.markdown(
@@ -135,5 +202,15 @@ st.markdown(
     f"""*Global mean sea level anomaly (Graph 2)*  \nCopernicus Climate Change Service. 
     Climate indicators - Sea level: Figure 1. Daily change in global mean sea level 
     [Dataset]. https://climate.copernicus.eu/climate-indicators/sea-level.
+    Date Accessed {date.today()}."""
+)
+st.markdown(
+    """*Global average pH estimation (Graph 3)*  \nCopernicus Marine Service. 
+    Ocean Climate Portal - Ocean Acidification. [Dataset]. https://marine.copernicus.eu/ocean-climate-portal/ocean-acidification.
+    Date Accessed 2025-10-04."""
+)
+st.markdown(
+    f"""*Measured pH levels from Aloha Station Hawaii (Graph 3)*  \nHawaii Ocean Time-series (HOT).  
+    [Dataset]. https://hahana.soest.hawaii.edu/hot/hotco2/hotco2.html.
     Date Accessed {date.today()}."""
 )
